3d13b8369156da715235b5ad129e235810b068e9
Esteban Felipe <esteban.felipe@gmail.com>
2018-01-31 21:30:25 -0600

    Added custom methods

--- common/models/executionerror.js
+++ common/models/executionerror.js
@@ -1,1 +1,39 @@
-'use strict';module.exports=function(executionerror) {};
+'use strict';
+module.exports=function(executionerror) {
+  executionerror.getActiveTechnical = function(cb){
+    executionerror.find({where:{and: [{technicalerror:true},{resolved:false}]}},function(err,results){
+      cb(null,results)
+    })
+  }
+  executionerror.getResolvedTechnical = function(cb){
+    executionerror.find({where:{and: [{technicalerror:true},{resolved:true}]}},function(err,results){
+      cb(null,results)
+    })
+  }
+  executionerror.getActiveFunctional = function(cb){
+    executionerror.find({where:{and: [{technicalerror:false},{resolved:false}]}},function(err,results){
+      cb(null,results)
+    })
+  }
+  executionerror.getResolvedFunctional = function(cb){
+    executionerror.find({where:{and: [{technicalerror:false},{resolved:true}]}},function(err,results){
+      cb(null,results)
+    })
+  }
+  executionerror.remoteMethod('getActiveTechnical',{
+    http: { verb: 'get'},
+    returns: {arg: 'results',type :'executionerrors'}
+  })
+  executionerror.remoteMethod('getResolvedTechnical',{
+    http: { verb: 'get'},
+    returns: {arg: 'results',type :'executionerrors'}
+  })
+  executionerror.remoteMethod('getActiveFunctional',{
+    http: { verb: 'get'},
+    returns: {arg: 'results',type :'executionerrors'}
+  })
+  executionerror.remoteMethod('getResolvedFunctional',{
+    http: { verb: 'get'},
+    returns: {arg: 'results',type :'executionerrors'}
+  })
+};
--- common/models/launchpointcase.js
+++ common/models/launchpointcase.js
@@ -1,1 +1,75 @@
-'use strict';module.exports=function(launchpointcase) {};
+'use strict';
+
+module.exports=function(launchpointcase) {
+  launchpointcase.getProcesses = function(environmentid,clientid,caseid,cb){
+    var app = launchpointcase.app;
+    var casetracking = app.models.casetracking;
+    var iqtracking = app.models.iqtracking;
+    var isotracking = app.models.isotracking;
+    var executionerror = app.models.executionerror;
+    var results ={}
+    var query= {
+      where:{
+        and:[
+          {environmentid:environmentid},
+          {clientid:clientid},
+          {caseid:caseid}
+        ]
+      }
+    }
+    var queryct = query;
+    queryct.where.and.push({haschanges: true})
+
+    var queryex = query;
+    queryex.where.and.push({resolved:false})
+
+    casetracking.find(queryct,function(err,ct){
+      results.casetrackings = ct;
+      iqtracking.find(query,function(err,iqt){
+        results.iqtrackings = iqt;
+        isotracking.find(query,function(err,isot){
+          results.isotrackings = isot;
+
+          executionerror.find(queryex,function(err,exerr){
+            results.executionerrors = exerr
+            cb(null,results)
+          })
+        })
+      })
+    })
+  }
+  launchpointcase.getCase = function(environmentid,clientid,caseid,cb){
+    var query= {
+      where:{
+        and:[
+          {environmentid:environmentid},
+          {clientid:clientid},
+          {caseid:caseid}
+        ]
+      }
+    }
+
+    launchpointcase.find(query,function(err,lp){
+      if(lp && lp.length > 0){
+        cb(null,lp[0])
+      }
+      else{
+        cb(null,{})
+      }
+    });
+  }
+
+  launchpointcase.remoteMethod('getCase',{
+    accepts:[
+      { arg : 'environmentid', type:'integer'},{arg:'clientid',type:'integer'},{arg:'caseid',type:'integer'}],
+    http: { path:'/getcase/:environmentid/:clientid/:caseid', verb: 'get'},
+    returns: {arg: 'launchpointcase',type :'Object'}
+  })
+  launchpointcase.remoteMethod('getProcesses',{
+    accepts:[
+      { arg : 'environmentid', type:'integer'},{arg:'clientid',type:'integer'},{arg:'caseid',type:'integer'}],
+    http: { path:'/getprocesses/:environmentid/:clientid/:caseid', verb: 'get'},
+    returns: {arg: 'results',type :'Object'}
+  })
+
+};
